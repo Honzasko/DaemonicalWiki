9d2627b943101bcfc782cd5173b40b981be613d5
Jan <straka.jan5@seznam.cz>
2018-04-08 14:37:33 +0200

    Add files via upload

---
+++ mlg_cons.py
@@ -0,0 +1,21 @@
+print("Welcome to MLG Console")
+print("v1.1 Beta") 
+
+while(True):
+    input = raw_input("MLG_Command:")
+
+    if input == 'help':
+       print("pls,your,money,dog,win")
+    if input == 'pls':
+       print("Please give me your penis")
+    if input == 'your':
+       print("Fuck your bitch")
+    if input == 'money':
+       print("You are not rich,fuck you bitch")    
+    if input == 'dog':
+       print("DOGE!!!")    
+       print("DOGE!!!")
+       print("DOGE!!!")
+    if input == 'win':
+       print("Windows is SH*T and Linux is BEST")
+    
